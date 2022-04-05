526b91c5a96f2bb745d95b5cfa995bc0e8ce7e8c
Ramiza1998 <ramizayakubu994@gmail.com>
2022-04-05 09:41:15 -0700

    added some new data

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,19 +1,19 @@
 import React from 'react'
-import {FaJediOrder } from "react-icons/fa"
-import {Link} from "react-router-dom"
+import {FaApple} from "react-icons/fa"
+import { Link } from 'react-router-dom'
+
 function NavBar() {
   return (
     <div className='header'>
-    <FaJediOrder className='icon'/>
-    <h2>Target Phones And Accessories</h2>
+    <FaApple className= 'icons'/>
+    <h2>Ramiza Phones And Accessories</h2>
     <ul>
-        <li> <Link to ="/">Home</Link> </li>
-        <li> <Link to ="/about">About Us</Link> </li>
-        <li> <Link to = "/products">Products</Link> </li>
-        <li> <Link to = "/contact us">Contact Us</Link> </li>
+    <li> <Link to ="/">Home</Link></li>
+    <li><Link to ="/about">About us</Link></li>
+    <li><Link to ="/products">Products</Link></li>
+    <li><Link to ="/contact">Contact us</Link></li>
+    
     </ul>
-  
-  
 
     </div>
   )
